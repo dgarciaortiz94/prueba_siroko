79535519baa740020a339658729ce27e346518d7
Diego García <dgarciaortiz94@gmail.com>
2024-02-26 21:36:31 +0100

    feat: add searchProduct method to CartRepository adapter

--- src/Dashboard/Cart/Domain/Persist/ICartRepository.php
+++ src/Dashboard/Cart/Domain/Persist/ICartRepository.php
@@ -3,14 +3,15 @@
 namespace App\Dashboard\Cart\Domain\Persist;
 
 use App\Dashboard\Cart\Domain\Aggregate\Cart;
+use App\Dashboard\Cart\Domain\Aggregate\CartItem\CartItemProduct\CartItemProduct;
 
 interface ICartRepository
 {
-    public function save(): Cart;
+    public function save(Cart $cart): Cart;
 
     public function remove(Cart $cart): void;
 
     public function search(string $id): Cart;
 
-    public function searchByCriteria(array $criteria): array;
+    public function searchProduct(string $id): CartItemProduct;
 }
