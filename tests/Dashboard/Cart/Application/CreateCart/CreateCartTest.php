675df6ed75a8767e1b4c6178bd651eb23fb0f567
Diego García <dgarciaortiz94@gmail.com>
2024-02-26 22:09:02 +0100

    test: add create cart successfully test case

--- tests/Dashboard/Cart/Application/CreateCart/CreateCartTest.php
+++ tests/Dashboard/Cart/Application/CreateCart/CreateCartTest.php
@@ -2,6 +2,35 @@
 
 namespace App\Tests\Dashboard\Cart\Application\CreateCart;
 
-class CreateCartTest
+use App\Dashboard\Cart\Application\CreateCart\CreateCartCase;
+use App\Dashboard\Cart\Application\CreateCart\CreateCartResponse;
+use App\Dashboard\Cart\Domain\Services\CartCreator;
+use App\Tests\Dashboard\Cart\Application\AbstractCartApplicationMock;
+use App\Tests\Dashboard\Cart\Domain\CartItemMother;
+use App\Tests\Dashboard\Cart\Domain\CartMother;
+
+class CreateCartTest extends AbstractCartApplicationMock
 {
+    /**
+     * @test
+     */
+    public function shouldCreateCartSuccessfully()
+    {
+        $cart = CartMother::create(
+            CartItemMother::create(),
+        );
+
+        $this->repository()->expects($this->once())
+            ->method('save')
+            ->willReturn($cart);
+
+        $createCartCase = new CreateCartCase(
+            new CartCreator($this->repository()),
+            $this->eventBus()
+        );
+
+        $createCartResponse = $createCartCase->__invoke($cart);
+
+        $this->assertEquals(CreateCartResponse::class, $createCartResponse::class);
+    }
 }
