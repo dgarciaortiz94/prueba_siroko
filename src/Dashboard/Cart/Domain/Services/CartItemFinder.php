cb249c45475671ef7bc66ae96487e55d16fdb811
Diego García <dgarciaortiz94@gmail.com>
2024-02-28 23:08:34 +0100

    feat: add cart item finder by id domain service

---
+++ src/Dashboard/Cart/Domain/Services/CartItemFinder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Dashboard\Cart\Domain\Services;
+
+use App\Dashboard\Cart\Domain\Aggregate\Cart;
+use App\Dashboard\Cart\Domain\Aggregate\CartId;
+use App\Dashboard\Cart\Domain\Aggregate\CartItem\CartItem;
+use App\Dashboard\Cart\Domain\Aggregate\CartItem\CartItemId;
+use App\Dashboard\Cart\Domain\Persist\ICartRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CartItemFinder
+{
+    public function __construct(private ICartRepository $repository)
+    {
+    }
+
+    public function __invoke(CartItemId $id): CartItem
+    {
+        $item = $this->repository->searchItem($id->value());
+
+        $this->checkItemExists($item);
+
+        return $item;
+    }
+
+    private function checkItemExists(?CartItem $item): void
+    {
+        if (!$item) {
+            throw new NotFoundHttpException('Item not found by this id');
+        }
+    }
+}
