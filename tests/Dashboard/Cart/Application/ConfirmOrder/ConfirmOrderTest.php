9c5bed46d1b2345e65bc6cb6159b2c297ded71af
Diego García <dgarciaortiz94@gmail.com>
2024-03-03 13:39:32 +0100

    test: create test for case OrderAlreadyCreatedByThisCartException

--- tests/Dashboard/Cart/Application/ConfirmOrder/ConfirmOrderTest.php
+++ tests/Dashboard/Cart/Application/ConfirmOrder/ConfirmOrderTest.php
@@ -13,8 +13,10 @@ use App\Dashboard\Cart\Domain\Aggregate\CartOrder\CartOrderAddressData\CartOrder
 use App\Dashboard\Cart\Domain\Aggregate\CartOrder\CartOrderAddressData\CartOrderAddressDataZipCode;
 use App\Dashboard\Cart\Domain\Aggregate\CartOrder\CartOrderPaymentData\CartOrderPaymentDataCard;
 use App\Dashboard\Cart\Domain\Exception\NoItemsInCartException;
+use App\Dashboard\Cart\Domain\Exception\OrderAlreadyCreatedForThisCartException;
 use App\Dashboard\Cart\Domain\Services\CartFinder;
 use App\Shared\Domain\Services\CurrentUserRecovery;
+use App\Shared\Domain\Validation\Payment\InvalidPaymentCardException;
 use App\Tests\Dashboard\Cart\Application\AbstractCartApplicationMock;
 use App\Tests\Dashboard\Cart\Domain\CartItemMother;
 use App\Tests\Dashboard\Cart\Domain\CartMother;
@@ -108,6 +110,91 @@ class ConfirmOrderTest extends AbstractCartApplicationMock
         );
 
         $this->expectException(NoItemsInCartException::class);
+
+        $confirmOrder->__invoke(
+            new CartId('018dec59-16b4-7529-8953-adf9194d5888'),
+            new CartOrderPaymentDataCard('4485-2465-4898-2545'),
+            new CartOrderAddressDataAddress('Calle Falsa 123'),
+            new CartOrderAddressDataLocation('Alcorcón'),
+            new CartOrderAddressDataComunity('Madrid'),
+            new CartOrderAddressDataZipCode('28015'),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnInvalidPaymentCardException()
+    {
+        $currentUserRecovery = $this->currentUserRecovery();
+        $repositoryCartFound = $this->repository();
+        $repositoryCartSave = $this->repository();
+
+        $confirmOrder = new ConfirmOrderCase(
+            new CartFinder($repositoryCartFound),
+            new CartOrderConfirmer($repositoryCartSave),
+            $this->eventBus(),
+            $currentUserRecovery
+        );
+
+        $this->expectException(InvalidPaymentCardException::class);
+
+        $confirmOrder->__invoke(
+            new CartId('018dec59-16b4-7529-8953-adf9194d5888'),
+            new CartOrderPaymentDataCard('GFDF-2465-4898-2545'),
+            new CartOrderAddressDataAddress('Calle Falsa 123'),
+            new CartOrderAddressDataLocation('Alcorcón'),
+            new CartOrderAddressDataComunity('Madrid'),
+            new CartOrderAddressDataZipCode('28015'),
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldOrderAlreadyCreatedByThisCartException()
+    {
+        $item = CartItemMother::create(
+            product: CartProductMother::create()
+        );
+
+        $cart = CartMother::create($item);
+
+        $currentUserRecovery = $this->currentUserRecovery();
+        $repositoryCartFound = $this->repository();
+        $repositoryCartSave = $this->repository();
+
+        $currentUserRecovery
+            ->expects($this->exactly(2))
+            ->method('__invoke')
+            ->willReturn(null);
+
+        $repositoryCartFound
+            ->expects($this->exactly(2))
+            ->method('search')
+            ->willReturn($cart);
+
+        $repositoryCartSave->expects($this->once())
+            ->method('save')
+            ->willReturn($cart);
+
+        $confirmOrder = new ConfirmOrderCase(
+            new CartFinder($repositoryCartFound),
+            new CartOrderConfirmer($repositoryCartSave),
+            $this->eventBus(),
+            $currentUserRecovery
+        );
+
+        $confirmOrder->__invoke(
+            new CartId('018dec59-16b4-7529-8953-adf9194d5888'),
+            new CartOrderPaymentDataCard('4485-2465-4898-2545'),
+            new CartOrderAddressDataAddress('Calle Falsa 123'),
+            new CartOrderAddressDataLocation('Alcorcón'),
+            new CartOrderAddressDataComunity('Madrid'),
+            new CartOrderAddressDataZipCode('28015'),
+        );
+
+        $this->expectException(OrderAlreadyCreatedForThisCartException::class);
 
         $confirmOrder->__invoke(
             new CartId('018dec59-16b4-7529-8953-adf9194d5888'),
